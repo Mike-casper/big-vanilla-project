407d0adbb692f41b5a3cb634b5e8accce3f617a1
Mike-casper <misha7709123@gmail.com>
2022-12-18 22:14:17 +0300

    Поработали с объектами и типизашкой

---
+++ src/02/02.ts
@@ -0,0 +1,53 @@
+type StudentType = {
+    name: string,
+    age: number,
+    isActive: boolean,
+    address:AdressType
+    technologies:Array<TechType>
+}
+type AdressType = {
+    streetTitle: string,
+    city: LocalCityType,
+}
+type LocalCityType = {
+    title:string
+    countryTitle:string
+}
+
+type TechType = {
+    id: number
+    title:string
+}
+
+const student: StudentType = {
+    "name": "Mike",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "JS"
+        }
+    ]
+}
+console.log(student.address.city)
+console.log(student.technologies[2].id)
+
+
+
+
