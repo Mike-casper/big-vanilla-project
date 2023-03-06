3b0668c8b802073cabc7313f5f8e5d27a50e6666
Mike-casper <misha7709123@gmail.com>
2023-03-06 13:17:23 +0300

    # 8 Ассоциативный массив

--- src/08-Associative-Array/08-array.ts
+++ src/08-Associative-Array/08-array.ts
@@ -1,16 +1,28 @@
+import {logDOM} from "@testing-library/react";
+
 export const usersObj = {
     "0": "Dimych",
     "1": "Margo",
     "2": "Nata",
     "3": "Kate",
 }
+type UsersType = {
+    [key: string] : {id:number, name:string}
+}
 
-export const users = {
+export const users:UsersType = {
     "101": {id: 101, name: "Dima"},
     "2222": {id: 2222, name: "Kate"},
     "1021": {id: 1021, name: "Dara"},
     "1101": {id: 1101, name: "Anna"},
 }
+
+// users[1101]
+let user = {id:100500, name: "Igor"}
+users[user.id] = user;
+delete users[user.id]
+console.log(users)
+users[user.id].name = "Vitya"
 
 export const usersArray = [
     {id: 101, name: "Dima"},
@@ -18,3 +30,5 @@ export const usersArray = [
     {id: 1021, name: "Dara"},
     {id: 1101, name: "Anna"}
     ]
+
+// usersArray.find(u=>u.id === 1)
--- src/08-Associative-Array/08_01.test.ts
+++ src/08-Associative-Array/08_01.test.ts
@@ -1,14 +1,30 @@
-import {usersObj} from "./08-array";
-
+// import {usersObj} from "./08-array";
+type UsersType = {
+    [key: string] : {id:number, name:string}
+}
+let users: UsersType
 
 beforeEach(()=>{
-
+users = {
+    "101": {id: 101, name: "Dima"},
+    "2222": {id: 2222, name: "Kate"},
+    "1021": {id: 1021, name: "Dara"},
+    "1101": {id: 1101, name: "Anna"},
+}
 
 })
 
 test("should select corresponding user from obj",()=>{
-    expect(usersObj[0]).toBe("Dima");
-    expect(usersObj[0]).toBe("Nata");
-    expect(usersObj[0]).toBe("Dara");
-    expect(usersObj[0]).toBe("Kate");
+    users["101"].name = "D"
+    expect(users["101"].name).toBe("D");
+    expect(users["2222"]).toEqual({id: 2222, name: "Kate"});
+    expect(users["1021"]).toEqual({id: 1021, name: "Dara"});
+    expect(users["1101"]).toEqual({id: 1101, name: "Anna"});
 })
+
+test("should delete corresponding user from obj",()=>{
+    delete users["101"]
+    expect(users["101"]).toBeUndefined();
+})
+
+
